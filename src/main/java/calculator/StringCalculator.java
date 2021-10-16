753c3463118c7879b28d8b508dae3f15e4417c94
yogendra7 <iamyogendrayadav7@gmail.com>
2021-10-17 00:44:28 +0530

    Check for empty and check for single number

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,11 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+       if(input.isEmpty()) {
+    	   return 0;
+       }else {
+    	   return Integer.parseInt(input);
+       }
     }
 
 }
